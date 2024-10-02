9699a847c25e7012c8ef2a6ace00443d4b3ff399
HieuNghia6572 <phanhieunghia1054@gmail.com>
2024-10-02 13:32:17 +0700

    Initial commit

---
+++ TracNghiem/src/main/java/com/example/TracNghiem/TracNghiemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TracNghiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracNghiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracNghiemApplication.class, args);
+	}
+
+}
---
+++ TracNghiem/src/test/java/com/example/TracNghiem/TracNghiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TracNghiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracNghiemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
