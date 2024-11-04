a5a1f446283795850c6a03aa5ca18d07a5ebb54b
ductai <taiduc863@gmail.com>
2024-11-05 06:06:00 +0700

    thêm thông báo

--- TracNghiem/src/main/java/com/example/TracNghiem/controller/CustomerController.java
+++ TracNghiem/src/main/java/com/example/TracNghiem/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.example.TracNghiem.controller;
 
 import com.example.TracNghiem.services.CauHoiService;
+import com.example.TracNghiem.services.ThongBaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CustomerController {
     @Autowired
     private CauHoiService cauHoiService;
+    @Autowired
+    private ThongBaoService thongBaoService;
     @GetMapping("/home")
     public String showCauhoiList(Model model) {
         model.addAttribute("cauhois", cauHoiService.getAllCauHoi());
+
+
+
+        model.addAttribute("thongbaos", thongBaoService.getAllThongBao());
         return "/userlayout";
     }
 }
