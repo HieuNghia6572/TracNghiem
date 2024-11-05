abda7460314281f51be3ba6fab2f104fee4bac90
HieuNghia6572 <phanhieunghia1054@gmail.com>
2024-11-05 21:20:32 +0700

    chinh sua trang chu

--- TracNghiem/src/main/java/com/example/TracNghiem/controller/CustomerController.java
+++ TracNghiem/src/main/java/com/example/TracNghiem/controller/CustomerController.java
@@ -1,6 +1,9 @@
 package com.example.TracNghiem.controller;
 
+import com.example.TracNghiem.entity.CauHoi;
+import com.example.TracNghiem.entity.MonThi;
 import com.example.TracNghiem.services.CauHoiService;
+import com.example.TracNghiem.services.MonThiService;
 import com.example.TracNghiem.services.ThongBaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class CustomerController {
+    @Autowired
+    private MonThiService monThiService;
     @Autowired
     private CauHoiService cauHoiService;
     @Autowired
@@ -18,10 +25,11 @@ public class CustomerController {
     @GetMapping("/home")
     public String showCauhoiList(Model model) {
         model.addAttribute("cauhois", cauHoiService.getAllCauHoi());
-
-
-
         model.addAttribute("thongbaos", thongBaoService.getAllThongBao());
-        return "/userlayout";
+        List<MonThi> monThis = monThiService.getAllMonThi();
+        List<CauHoi> cauHois = cauHoiService.getAllCauHoi();
+        model.addAttribute("monthis", monThis);
+        model.addAttribute("cauhois", cauHois);
+        return "/userlayout"; // Đường dẫn tới template
     }
 }
